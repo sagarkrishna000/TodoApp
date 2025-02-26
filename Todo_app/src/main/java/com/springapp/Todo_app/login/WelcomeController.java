310e8091412d15520681a08db79012a8c0459b52
sagarkrishna000 <sagarkrishna000@gmail.com>
2025-02-27 02:13:49 +0530

    Spring Security Added along with Hardcode cleanup

---
+++ Todo_app/src/main/java/com/springapp/Todo_app/login/WelcomeController.java
@@ -0,0 +1,20 @@
+package com.springapp.Todo_app.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name")
+public class WelcomeController {
+	
+
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String loadWelcomePage(ModelMap model) {
+		model.put("name", "Sagar");
+		return "welcome";
+	}
+}
